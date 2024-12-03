a3a7190fffbd3bdb0d045bdecd50cbe06e676cfa
Dennis Mronga <dennis.mronga@dfki.de>
2024-12-03 10:06:18 +0100

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,3 +1,9 @@
-# For Python bindings
+# Requirements
 sudo apt-get -y install python3-dev python3-numpy python3-nose libboost-python-dev libboost-numpy-dev
+git clone git@github.com:ARC-OPT/wbc_py.git
+mkdir wbc_py/build
+cd wbc_py/build
+cmake .. & make -j8
+sudo make install
+
 
